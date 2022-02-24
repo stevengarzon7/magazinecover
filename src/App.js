eb61b1ea86598f394eb41ba61c6aa97f3052cf11
Steven Garzon Reina <stevengarzonreina@iMac-de-Steven.local>
2022-02-24 09:26:11 -0500

    magazinecover

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
+import magazineBackground from './zuckerberg-testify.jpg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="main-container">
+        <section className="top-info">
+            <p className="info">United States</p>
+            <p className="info">FEB. 24 / MARCH. 1, 2022</p>
+        </section>
+
+        <article className="magazine-cover">
+            <header className="title-container">
+                <h1 className="magazine-title">TIME</h1>
+            </header>
+
+            <figure className="img-container">
+                <img src={magazineBackground} alt="" className="background-img"/>
+            </figure>
+
+            
+
+            <footer className="articles-container">
+                <article className="article">
+                    <p className="name">Mark Zuckerberg</p>
+                    <p className="author">Meta CEO</p>
+                </article>
+
+                <article className="article">
+                    <p className="name">CHANGE THE WORLD</p>
+                    <p className="author">METAVERSE</p>
+                </article>
+            </footer>
+        </article>
+
+        <section className="bottom-info">
+            <p className="info">time.com</p>
+        </section>
+    </main>
   );
 }
 
